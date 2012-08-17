2c6207dcceaff251c89266789fd04dce36470428
Steve Bidenko <steve.bidenko@huffingtonpost.com>
2012-08-17 10:47:21 -0400

    #1 modified the first sample and added the second sample

--- perl/1.pl
+++ perl/1.pl
@@ -1,3 +1,7 @@
 #!/usr/bin/perl -w
 
-print "Arguments is @ARGV\n";
+if (@ARGV) {
+	print "Arguments is @ARGV. Number of them is ".scalar(@ARGV)."\n";
+} else {
+	print "Number of arguments is equal 0\n";
+}
