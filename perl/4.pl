9c2a80d71e87618c0cf8f9442aea9a3c40fb3bd4
sterebooster <stereobooster@gmail.com>
2012-09-13 23:45:34 +0300

    Revert "Merge pull request #11 from SteveBidenko/BASH-1"
    This reverts commit 489d2af934bb24eb3f993447bc7da643036feaa1, reversing
    changes made to 7219de5d35943e7759477c863bb9bfc420b8c47a.

--- perl/4.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl -w
-#
-printf "This is sample string with formatted. X = %d\n", 25;
-print "Added this line for PHP-1 branch\n";
--- php/1.php
+++ php/1.php
@@ -4,4 +4,3 @@ $b = 'x';
 var_dump(FALSE == $a); //result TRUE
 var_dump($a == $b); //result TRUE
 var_dump($b == TRUE); //result TRUE
-var_dump(array($a, $b));
